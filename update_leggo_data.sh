ddb1370c485be0e290cc8a705bd18cadf61b18e7
gileadekelvin <gileadekelvin@gmail.com>
2020-04-01 17:20:03 -0300

    Adiciona parâmetro para recuperar apelido dos interesses na pressão

--- update_leggo_data.sh
+++ update_leggo_data.sh
@@ -168,6 +168,7 @@ pprint "Gerando dataframe com os apelidos para busca no Twitter e Google Trends"
 docker-compose -f $LEGGOTRENDS_FOLDERPATH/docker-compose.yml run --rm leggo-trends \
        Rscript gera_entrada_google_trends.R \
        -p leggo_data/proposicoes.csv \
+       -i leggo_data/interesses.csv \
        -a leggo_data/apelidos.csv
 check_errs $? "Não foi possível gerar os dados de apelidos das proposições."
 
